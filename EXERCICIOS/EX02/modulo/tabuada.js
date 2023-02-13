b4f1080e19a0a233fe2adbfa095baac99c3def3f
VINICIUSNUNES137 <90266473+VINICIUSNUNES137@users.noreply.github.com>
2023-02-12 23:44:22 -0300

    tabuada

---
+++ EXERCICIOS/EX02/modulo/tabuada.js
@@ -0,0 +1,25 @@
+const tabuada = function (
+  multiplicandoMin,
+  multiplicandoMax,
+  multiplicadorMin,
+  multiplicadorMax
+) {
+  let minMultiplicando = multiplicandoMin
+  let maxMultiplicando = multiplicandoMax
+  let minMultiplicador = multiplicadorMin
+  let maxMultiplicador = multiplicadorMax
+  let contador
+
+  while (minMultiplicando <= maxMultiplicando) {
+    console.log(`\n-----------------${minMultiplicando}-----------------\n`)
+    contador = minMultiplicador
+    while (contador <= maxMultiplicador) {
+      let resultado = minMultiplicando * contador
+      console.log(`${minMultiplicando} x ${contador} = ${resultado}`)
+      contador++
+    }
+    minMultiplicando++
+  }
+}
+
+tabuada(4, 7, 1, 10)
